e5849548b1704f45796dd4a8bd8bd8908f0af827
Oleksii <alex.hitrik@gmail.com>
2017-05-02 19:39:27 +0300

    optimization code

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
-
 function myFunction() {
 	    var x = document.getElementById("myTopnav");
 		var y = document.getElementById("heder_text_id");
 	    if (x.className === "topnav") {
-	        x.className += " responsive";
-	        y.className += " responsive";
-	    } else {
+	        x.className += "responsive";
+	        y.className += "responsive";
+	    } 
+	    else {
 	        x.className = "topnav";
 	        y.className = "heder_text";
 	    }
